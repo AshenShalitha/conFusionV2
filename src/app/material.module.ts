7ca9cf319be83ddb2d19729ba152d395928afb3e
Ashen Shalitha <ashenshalitha39@gmail.com>
2018-05-05 16:31:17 +0530

    Assignment 1

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatToolbarModule, MatListModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatListModule, MatGridListModule, MatCardModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatListModule],
-  exports: [MatButtonModule, MatToolbarModule, MatListModule],
+  imports: [MatButtonModule, MatToolbarModule, MatListModule, MatGridListModule, MatCardModule],
+  exports: [MatButtonModule, MatToolbarModule, MatListModule, MatGridListModule, MatCardModule],
 })
 export class MaterialModule { }
